4c36265cd24430ec80c59abdac487a1c36f1b3e7
Shita Tamanaha <e195751@ie.u-ryukyu.ac.jp>
2020-01-20 16:14:45 +0900

    5回目　ユニットテスト

---
+++ src/test/java/ie/u/ryukyu/e195751/Report6Test.java
@@ -0,0 +1,12 @@
+package ie.u.ryukyu.e195751;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Report6Test {
+
+    @Test
+    void main() {
+    }
+}
