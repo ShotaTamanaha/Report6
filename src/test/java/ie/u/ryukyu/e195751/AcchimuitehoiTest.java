c020b0a77cfb89583aa23a0486d795d339d66c58
Shita Tamanaha <e195751@ie.u-ryukyu.ac.jp>
2020-01-21 23:57:37 +0900

    7回目　ユニットテスト

--- src/test/java/ie/u/ryukyu/e195751/AcchimuitehoiTest.java
+++ src/test/java/ie/u/ryukyu/e195751/AcchimuitehoiTest.java
@@ -3,6 +3,7 @@ package ie.u.ryukyu.e195751;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
 
 class AcchimuitehoiTest {
 
@@ -24,13 +25,16 @@ class AcchimuitehoiTest {
 
     @Test
     void player1_win_and_player2_lose_judge() {
-        int player1_pattern = 3;
-        int player2_pattern = 2;
+        Acchimuitehoi n = new Acchimuitehoi();
+        n.player1_patten = 2;
+        n.player2_pattern = 2;
+        assertEquals(n.player1_patten,n.player2_pattern);
     }
-
     @Test
     void player1_lose_and_player2_win_judge() {
-        int player1_pattern = 1;
-        int player2_pattern = 1;
-    }
-}
+
+            Acchimuitehoi k = new Acchimuitehoi();
+            k.player1_patten=1;
+            k.player2_pattern=1;
+            assertEquals(k.player1_patten,k.player2_pattern);
+}}
