c8c092f7c21202a0a82e131e5beb608a2716f2ae
Shita Tamanaha <e195751@ie.u-ryukyu.ac.jp>
2020-01-21 23:06:11 +0900

    5回目　ユニットテスト

--- src/main/java/ie/u/ryukyu/e195751/Report6.java
+++ src/main/java/ie/u/ryukyu/e195751/Report6.java
@@ -42,46 +42,65 @@ class junken {
     int player1;
     int player2;
 
-    //プレイヤー１は自分なので自分で手を出すのを決めるメソッド
+    /**
+     * プレイヤー１のジャンケンを決めるためのメソッド
+     * 出した数字の大きさによって、グー、チョキ、パーを出したか決定する。
+     */
     public void decide_player1() {
         try {
             Scanner scan = new Scanner(System.in);
             System.out.println("出力してください");
             int hand1 = scan.nextInt();
-            if (hand1 == Rock) {
-                System.out.println("player1はグーを出しました。");
-                player1 = Rock; }
-            else if (hand1 == paper) {
-                System.out.println("player1はパーを出しました。");
-                player1 = paper; }
-            else if (hand1 == scissors) {
-                System.out.println("player1はチョキを出しました。");
-                player1 = scissors; }
-            else {
-                System.out.println("0,1,2以外なので実行できません"); }
+            switch (hand1){
+                case 0:
+                    System.out.println("プレイヤー１はグーを出しました。");
+                    player1 = Rock;
+                    break;
+                case 1:
+                    System.out.println("player1はパーを出しました。");
+                    player1 = paper;
+                    break;
+                case 2:
+                    System.out.println("player1はチョキを出しました。");
+                    player1 = scissors;
+                    break;
+                default:
+                    System.out.println("0,1,2以外の数字は実行できません");
+            }
         }catch (java.util.InputMismatchException x){
             System.out.println("文字列は読み込めません");
             System.out.println("もう一度やり直してください");
         }
     }
 
+    /**
+     * プレイヤー２（CPU)のジャンケン出し方について定義したメソッド
+     * CPUはコンピュータなのでコンピュータが出し方を決めるのでランダムクラスを使って
+     * 乱数でジャンケンの出し方を考えた。
+     */
     public void decide_player2() {
         Random random = new Random();
         int hand2 = random.nextInt(2);
-        if (hand2 == Rock) {
-            System.out.println("player2はグーを出しました。");
-            player2 = Rock;
-        } else if (hand2 == paper) {
-            System.out.println("player2はパーを出しました。");
-            player2 = paper;
-        } else if (hand2 == scissors) {
-            System.out.println("player2はチョキを出しました。");
-            player2 = scissors;
+        switch (hand2) {
+            case 0:
+                System.out.println("player2はグーを出しました。");
+                player2 = Rock;
+                break;
+            case 1:
+                System.out.println("player2はパーを出しました。");
+                player2 = paper;
+                break;
+            case 2:
+                System.out.println("player2はチョキを出しました。");
+                player2 = scissors;
+                break;
         }
     }
 
-
-    //ジャンケンの判定とスコアの起動を決めるためのメソッド
+    /**
+     *  ジャンケンの判定とスコアの起動を決めるためのメソッド
+     *  if文を使って勝ち、負け、引き分けについて考えた。
+     */
     public void Judge() {
         Acchimuitehoi t = new Acchimuitehoi();
         if (player1 == scissors & player2 == scissors) {
@@ -120,21 +139,32 @@ class junken {
             t.player1_win_pattern();
             t.player2_lose_pattern();
             t.player1_win_and_player2_lose_judge();}
-        else if(player1!=0&player1!=1&player1!=2) {
+        else if(player1>2&player2<3) {
             System.out.println("0,1,2以外の数字なので実行出来ません"); }
     }
 
 }
 
 class  Acchimuitehoi {
+
+    /**
+     * @param array1 勝った時
+     * @param array2 負けた時
+     */
     String[] array1 = {"あっち向いてホイ➡️", "あっち向いてホイ⬅️", "あっち向いてホイ⬆️", "あっち向いてホイ⬇️"};
     String[] array2 = {"➡️", "⬅️", "⬆️️", "⬇️️"};
 
     int n, t, k, b;
 
+    /**
+     * @param player1_pattern プレイヤー１のあっち向いてホイ
+     * @param player2_pattern  プレイヤー2のあっち向いてホイ
+     */
     int player1_patten, player2_pattern;
 
-
+    /**
+     * プレイヤー１がジャンケンで勝った時のあっち向いてほいのパターンのメソッド
+     */
     void player1_win_pattern() {
         try {
             System.out.println("ボタンを押してください");
@@ -158,8 +188,9 @@ class  Acchimuitehoi {
             System.out.println("もう一度やり直してください"); }
         }
 
-
-
+    /**
+     * プレイヤー2がジャンケンで勝った時のあっちむいてほいのパターンのメソッド
+     */
         void player2_win_pattern(){
             Random random = new Random();
             n = random.nextInt(3);
@@ -176,7 +207,9 @@ class  Acchimuitehoi {
                 System.out.println(array1[3]);
                 player2_pattern = 3;}
         }
-
+    /**
+     * プレイヤー2がジャンケンで負けた時のあっちむいてほいのパターンのメソッド
+     */
         void player2_lose_pattern(){
             Random random = new Random();
             b = random.nextInt(3);
@@ -195,14 +228,14 @@ class  Acchimuitehoi {
             }
         }
 
-        void player1_lose_pattern() {
+    /**
+     * プレイヤー１がジャンケンで負けた時のあっちむいてほいのパターンのメソッド
+     */
+    void player1_lose_pattern() {
             try {
-                System.out.println("ボタンを押してください");
-                Scanner v = new Scanner(System.in);
-                k = v.nextInt();
-                if (k == 0) {
+                if (k==0){
                     System.out.println(array2[0]);
-                    player1_patten = 0;
+                    player1_patten=0;
                 } else if (k == 1) {
                     System.out.println(array2[1]);
                     player1_patten = 1;
@@ -217,7 +250,10 @@ class  Acchimuitehoi {
                 System.out.println("もう一度やり直してください"); }
         }
 
-        void player1_win_and_player2_lose_judge () {
+    /**
+     * ジャンケンでプレイヤー１が勝ってプレイヤー２が負けた時のあっち向いてホイの判定メソッド
+     */
+    void player1_win_and_player2_lose_judge () {
             if (player1_patten == 0 & player2_pattern == 0 || player1_patten == 1 & player2_pattern == 1 || player1_patten == 2 & player2_pattern == 2 || player1_patten == 3 & player2_pattern == 3) {
                 System.out.println("プレイヤー１の勝ちです。");
             } else if (player1_patten == 0 & player2_pattern == 1 || player1_patten == 0 & player2_pattern == 2 || player1_patten == 0 & player2_pattern == 3) {
@@ -230,7 +266,9 @@ class  Acchimuitehoi {
                 System.out.println("今の勝負では勝敗がわかりませんでした。");
             }
         }
-
+    /**
+     * ジャンケンでプレイヤー2が勝ってプレイヤー1が負けた時のあっち向いてホイの判定メソッド
+     */
         void player1_lose_and_player2_win_judge () {
             if (player1_patten == 0 & player2_pattern == 0 || player1_patten == 1 & player2_pattern == 1 || player1_patten == 2 & player2_pattern == 2 || player1_patten == 3 & player2_pattern == 3) {
                 System.out.println("プレイヤー2の勝ちです。");
@@ -246,7 +284,6 @@ class  Acchimuitehoi {
         }
 
 }
-
 
 
 
